91165f08dca043eaadbeadbe3b03506064406f92
Rzvn-mhmdi <mohammadyrezvan@gmail.com>
2025-12-03 21:50:23 +0330

    Update PaymentProcessor.java

--- Step-02-with-OOD/PaymentProcessor.java
+++ Step-02-with-OOD/PaymentProcessor.java
@@ -1,6 +1,6 @@
 package services;
 
-class PaymentProcessor {
+public class PaymentProcessor {
     public void payByCard(double amount){ System.out.println("Paid by card: " + amount); }
     public void payByCash(double amount){ System.out.println("Paid by cash: " + amount); }
     public void payByPayPal(double amount){ System.out.println("Paid by PayPal: " + amount); }
