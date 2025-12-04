fe7098e171abcc335258809053c30353391c78eb
Rzvn-mhmdi <mohammadyrezvan@gmail.com>
2025-12-04 14:15:28 +0330

    Update EmailSender.java

--- Step-02-with-OOD/EmailSender.java
+++ Step-02-with-OOD/EmailSender.java
@@ -1,6 +1,7 @@
 package services;
 
 public class EmailSender implements MessageSender{
+    @Override
     public void sendMessage(String to, String message){
         System.out.println("Sending email to " + to + ": " + message);
     }
