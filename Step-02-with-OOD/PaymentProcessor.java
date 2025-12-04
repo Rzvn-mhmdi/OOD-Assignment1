713b76dd553490c17743f13e121c28c2b05949e9
fatemehMahjoub <fatemehmahjoub99@gmail.com>
2025-12-04 15:40:52 +0330

    Remove PayPalPaymentService class to add it in new file.
    Removed PayPalPaymentService implementation from PaymentService interface.

--- Step-02-with-OOD/PaymentProcessor.java
+++ Step-02-with-OOD/PaymentProcessor.java
@@ -3,10 +3,3 @@ package services;
 public interface PaymentService {
     void processPayment(double amount);
 }
-
-public class PayPalPaymentService implements PaymentService {
-    @Override
-    public void processPayment(double amount) {
-        System.out.println("Paid by PayPal: " + amount);
-    }
-}
