5d840cc3e60d204d3c650a3f3d720b8ec2e587fc
fatemehMahjoub <fatemehmahjoub99@gmail.com>
2025-12-04 15:38:53 +0330

    Remove CashPaymentService class to add it in new file.
    Removed CashPaymentService implementation from PaymentService.

--- Step-02-with-OOD/PaymentProcessor.java
+++ Step-02-with-OOD/PaymentProcessor.java
@@ -4,13 +4,6 @@ public interface PaymentService {
     void processPayment(double amount);
 }
 
-public class CashPaymentService implements PaymentService {
-    @Override
-    public void processPayment(double amount) {
-        System.out.println("Paid by cash: " + amount);
-    }
-}
-
 public class PayPalPaymentService implements PaymentService {
     @Override
     public void processPayment(double amount) {
