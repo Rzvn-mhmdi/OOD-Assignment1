3bd378a19e2fc642e94571613d375e7bffb7f247
fatemehMahjoub <fatemehmahjoub99@gmail.com>
2025-12-05 14:02:52 +0330

    Making changes to Main to align with changes to ReservationService that were made to establish RSP, OCP principles

--- Step-02-with-OOD/Main.java
+++ Step-02-with-OOD/Main.java
@@ -1,18 +1,27 @@
-import constants.PaymentMethods;
+import services.*; 
 import models.Customer;
 import models.LuxuryRoom;
-import constants.Notifier;
-import services.Reservation;
 import models.Room;
-import services.ReservationService;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args){
         Customer customer = new Customer("Ali", "ali@example.com","09124483765", "Paris");
         Room room = new LuxuryRoom("203", 150);
         Reservation res = new Reservation(room, customer, 2);
-
+        DiscountStrategy parisDiscount = new ParisDiscountStrategy();
+        List<DiscountStrategy> allDiscounts = List.of(parisDiscount); 
+        DiscountStrategy compositeDiscountService = new CompositeDiscountService(allDiscounts);
+        PaymentService paypalProcessor = new PayPalPaymentService(); 
+        MessageSender emailSender = new EmailSender();
+        InvoicePrinter printer = new InvoicePrinter(); 
         ReservationService service = new ReservationService();
-        service.makeReservation(res, PaymentMethods.PAYPAL, Notifier.EMAIL);
+        service.makeReservation(
+            res, 
+            paypalProcessor,         
+            emailSender,              
+            compositeDiscountService, 
+            printer                   
+        );
     }
 }
